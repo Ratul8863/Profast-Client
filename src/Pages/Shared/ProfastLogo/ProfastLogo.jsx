de92b6e174015a1def6fd3bfd557dcbcff54e9c5
Ratul8863 <ratulroy8863@gmail.com>
2025-07-13 23:14:57 +0600

    form 2

--- src/Pages/Shared/ProfastLogo/ProfastLogo.jsx
+++ src/Pages/Shared/ProfastLogo/ProfastLogo.jsx
@@ -7,7 +7,7 @@ function ProfastLogo() {
     <Link to={"/"}>
       <div className='flex items-center'>
         <img className='mb-4' src={logo} alt="" />
-        
+        <p className='text-3xl -ml-4 font-extrabold'>ProFast</p>
     </div>
     </Link>
   )
